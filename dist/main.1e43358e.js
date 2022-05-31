c4cc9caf9e394805222a01942fd42e145ecd015a
esracoskun <esranur.coskunn@gmail.com>
2022-05-31 09:48:39 +0300

    conflict

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -35328,7 +35328,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58562" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60132" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
